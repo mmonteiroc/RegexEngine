ff6e4650d00504cacb494497032e8b874734b523
miguel <miguelmonteiroclaveri@gmail.com>
2019-03-05 17:09:39 +0100

    Caracteres especiales

--- src/Find.java
+++ src/Find.java
@@ -1,7 +1,6 @@
-import java.util.List;
 /*
 * CARACTERES ESPECIALES
-* lo que usamos --> originales de regex
+* lo que usamos --> originales de regexOriginal
 * ? --> .
 * @ --> \
 * []
@@ -12,18 +11,60 @@ import java.util.List;
 *
 * */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Find {
+    private String texto;
+    private String regexOriginal;
+    private Pieza[] piezas;
 
-    public Find(String s) {
-
+    public Find(String texto) {
+        this.texto=texto;
     }
 
-    public boolean match(String stringPat) {
+    public boolean match(String regex) {
+        this.regexOriginal =regex;
+        piezas = Pieza.getPieza(regex);
 
 
         return false;
     }
-
 }
 
 
+class Pieza{
+
+    private enum Type{
+
+        cuantificador, caracterLiteral
+
+    }
+    private char car;
+    private Type type;
+
+    private Pieza(){}
+
+    private static Pieza car(char c){
+        Pieza p = new Pieza();
+        p.car = c;
+        p.type = Type.caracterLiteral;
+        return p;
+    }
+
+    static public Pieza[] getPieza(String expresion){
+        List<Pieza> devolver = new ArrayList<>();
+
+        for (int i = 0; i < expresion.length(); i++) {
+            if ((expresion.charAt(i)>=65 && expresion.charAt(i)<=90) | (expresion.charAt(i)>=97 && expresion.charAt(i)<=122) ){
+                // Letra
+                devolver = devolver.add(car(expresion.charAt(i)));
+
+            }
+        }
+
+
+
+        return devolver.toArray(new Pieza[0]);
+    }
+}
